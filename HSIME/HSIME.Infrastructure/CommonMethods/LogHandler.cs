c277f2306f6a7df0025ec1a25a22001f71dcdaac
rabbi2211 <rabbi2211@gmail.com>
2017-05-07 08:32:48 +0530

    Add files via upload

---
+++ HSIME/HSIME.Infrastructure/CommonMethods/LogHandler.cs
@@ -0,0 +1,18 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSIME.Infrastructure.CommonMethods
+{
+    public class LogHandler
+    {
+        private static Logger logger = null;
+        public static Logger LoggerInstance()
+        {
+            return logger = LogManager.GetCurrentClassLogger();
+        }
+    }
+}
